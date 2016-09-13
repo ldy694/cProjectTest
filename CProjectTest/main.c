e4f97e2ce4f6a7e1b54930a9c3934fff46b26ddd
swift_dong <linxingdong@foxmail.com>
2016-09-13 17:32:51 +0800

    Initial Commit

---
+++ CProjectTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CProjectTest
+//
+//  Created by 林兴栋 on 16/9/13.
+//  Copyright © 2016年 林兴栋. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
